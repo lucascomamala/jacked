657d237db7bf894ebe0a370b83f5879ce9856f6f
Lucas <lucascomamala@gmail.com>
2023-08-05 04:51:07 -0600

    index

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-import react from React;
-import reactDOM from ReactDOM;
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
