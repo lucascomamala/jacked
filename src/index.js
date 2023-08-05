fc9ddfeb324a82e8035b71137211837a9f035c98
Lucas <lucascomamala@gmail.com>
2023-08-05 04:53:03 -0600

    package

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
 import App from './App';
 
-root.render( <App />)
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
