d10613ba206b371e6fd16b16a322d4d18108e820
Lucas <lucascomamala@gmail.com>
2023-08-07 00:03:56 -0600

    Exercise Card

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import { useEffect, useState } from 'react';
+import { Box, Stack, Typography, Pagination } from '@mui/material';
 
-const Exercises = () => {
+import { exerciseOptions, fetchData } from '../utils/fetchData';
+import { ExerciseCard } from './';
+
+const Exercises = ({ exercises, setExercises, bodyPart }) => {
   return (
-    <div>Exercises</div>
+    <Box 
+      id="exercises"
+      sx={{mt: { lg: '110px' }}}
+      mt='50px'
+      p='20px'
+    >
+      <Typography variant='h3' mb='46px'>
+        Showing results
+      </Typography>
+      <Stack 
+        direction='row' 
+        sx={{ gap: {xs: '50px', lg: '110px'}}}
+        flexWrap='wrap'
+        justifyContent='center'
+      >
+        {exercises.map( (exercise, idx) => (
+          <ExerciseCard key='idx' exercise={exercise} />
+        ))}
+      </Stack>
+    </Box>
   )
 }
 
-export default Exercises
+export default Exercises;
