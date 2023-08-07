5a51bb05a2e60aaaa1b9ea72d989adc83f3a0a4f
Lucas <lucascomamala@gmail.com>
2023-08-07 12:00:49 -0600

    Footer

--- src/components/ExerciseVideos.js
+++ src/components/ExerciseVideos.js
@@ -1,8 +1,10 @@
 import { Box, Stack, Typography } from '@mui/material';
 
+import { Loader } from '.';
+
 const ExerciseVideos = ({ exerciseVideos, name}) => {
 
-  if (!exerciseVideos.length) return 'Loading...';
+  if (!exerciseVideos.length) return <Loader />;
 
   return (
     <Box sx={{ marginTop: { xs: '20px', lg: '100px' }}} p='20px'>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import { Box, Stack, Typography } from '@mui/material';
+
+import Logo from '../assets/images/Logo-1.png';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <Box mt='40px'>
+      <Stack gap='10px' alignItems='center' px='40px' pt='24px'>
+        <img src={Logo} alt='logo' width='200px' height='40px' />
+        <Typography variant='subtitle2' color='#b7b7b7'>© 2023 All rights reserved</Typography>
+      </Stack>
+    </Box>
   )
 }
 
-export default Footer
+export default Footer;
